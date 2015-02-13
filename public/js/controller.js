57b2bd8d735ff8188a92effb3a4faf1e614bd341
Joe Gaebel <jgaebel@uwo.ca>
2015-02-12 21:57:49 -0500

    added in sleek play button

--- public/js/controller.js
+++ public/js/controller.js
@@ -29,15 +29,33 @@ app.config(function($routeProvider, $locationProvider) {
 
 });
 
+$('#toHide').hide();
+
 function blur() {
     $('.shouldBlur').blur();
 }
+
+var play = false;
+
+function toggleTube() {
+    console.log('toggled!');
+    if (!play) {
+        $('#playbutton').attr('src', 'img/stop.svg');
+        $('#youtube').attr("src", "https://www.youtube.com/embed/NEpqlgAlvA8?enablejsapi=1&autoplay=1");
+    } else {
+        $('#playbutton').attr('src', 'img/play.svg');
+        $('#youtube').attr("src", "https://www.youtube.com/embed/NEpqlgAlvA8?enablejsapi=1");
+    }
+    play = !play;
+}
+
 
 app.controller('mainController', function($scope) {
 
     $scope.$on('$routeChangeSuccess', function(next, current) {
         blur();
     });
+
 
 
 
